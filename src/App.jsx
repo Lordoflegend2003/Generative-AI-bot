a85937bca404abdb593bc5be71cda21d013774ac
Vishal R K <rajasekarkavitha47@gmail.com>
2023-08-13 16:34:33 +0530

    rk

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello Word!
+    <h1 className='text-3xl font-bold underline text-center'>
+      Generative Bot
     </h1>
   )
 }
